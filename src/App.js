522885b86bc448b347cee3c97ec104a905f71eba
laoloser <slaux86@gmail.com>
2024-06-27 00:06:58 -0700

    final code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 
 function App() {
   return (
+    
     <div className="App">
+<Navbar bg="light" expand="lg" >
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src="[Instacart logo src]" /> 
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
+
       <h1>Hello World</h1>
+      <div id="bg-image">
+        <p>this do be a large image</p>
+        <Form>
+      <Form.Group className="mb-3" controlId="location">
+        <Form.Label>Where u at</Form.Label>
+        <Form.Control type="location" placeholder="Enter City or Zip Code" />
+
+      </Form.Group>
+      </Form>
+      </div>
+      <div>
+      <h2>Browse stores in Houston</h2>
+<Container style={{ marginTop: "40px", textAlign: "center" }}>
+    <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
+        <Row>
+            <Col xs=".5" >
+                <Image src="[IMG SRC]" roundedCircle />
+            </Col>
+            <Col>
+                <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+                <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+        <Col sm="1"></Col>
+        <Row >
+            <Col xs=".5">
+                <Image src="[IMG SRC]" roundedCircle />
+            </Col>
+            <Col>
+                <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+                <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+        <Col sm="1"></Col>
+        <Row >
+            <Col xs=".5" >
+                <Image src="[IMG SRC]" roundedCircle />
+            </Col>
+            <Col md="6">
+                <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
+                <p>Delivery</p>
+            </Col>
+        </Row>
+    </Row>
+</Container>
+</div>
     </div>
   );
 }
